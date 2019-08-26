8500b5db5868a27b7a16c5e766d3100aed362a31
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-08-26 11:28:29 +0530

    database connected

--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+// mongoose.Promise = global.Promise;
+//connect to mlab database
 
+mongoose.connect("mongodb://127.0.0.1:27017/todos", { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Mongodb database connection established successfully");
+});
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 app.use(
   "/graphql",
   graphqlHTTP({
