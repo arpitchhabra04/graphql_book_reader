5e064b972df80dabf56b0e3536d65c50f9f12c68
Arpit Chhabra <chhabra.arpi@gmail.com>
2019-08-27 03:22:24 +0530

    apollo set up

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import BookList from "./components/BookList";
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>List of Books</h1>
+        <BookList></BookList>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class BookList extends Component {
+  state = {};
+  render() {
+    return (
+      <div id="book-list">
+        <ul>
+          <li>Book Name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
